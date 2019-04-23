7d5c64af8ab0c0de09729977d6af591d795adccc
雨夜 <kiras0518@gmail.com>
2019-04-23 23:42:13 +0800

    init window

--- CodeLayout/AppDelegate.swift
+++ CodeLayout/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+
+        self.window?.backgroundColor = UIColor.blue
+ 
+        //設置根節點VC
+        self.window?.rootViewController = CustomTabBarController()
+        
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
