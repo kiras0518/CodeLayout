268d899e041fe7bba63f7a76b40e1726d97b180d
雨夜 <kiras0518@gmail.com>
2019-04-24 00:30:20 +0800

    新贈下方3個 imageView

--- CodeLayout/ViewController.swift
+++ CodeLayout/ViewController.swift
@@ -11,13 +11,14 @@ import UIKit
 class FeedCell: UITableViewCell {
     
     lazy var backView: UIView = {
-        let view = UIView(frame: CGRect(x: 10, y: 6, width: self.frame.width - 20, height: 110))
+        let view = UIView(frame: CGRect(x: 10, y: 6, width: self.frame.width - 20, height: self.frame.height - 20))
         view.backgroundColor = UIColor.green
         return view
     }()
     
     lazy var appImage: UIImageView = {
-        let image = UIImageView(frame: CGRect(x: 4, y: 4, width: 80, height: 80))
+        let image = UIImageView(frame: CGRect(x: 8, y: 8, width: 80, height: 80))
+        image.backgroundColor = UIColor.red
         return image
     }()
     
@@ -40,10 +41,31 @@ class FeedCell: UITableViewCell {
     }()
     
     lazy var getButton: UIButton = {
-        let button = UIButton(frame: CGRect(x: 300, y: 42, width: 40, height: 20))
+        let button = UIButton(frame: CGRect(x: 300, y: 42, width: 70, height: 30))
+        button.setTitle("Get", for: .normal)
+        button.backgroundColor = UIColor.darkGray
+        button.layer.cornerRadius = 15
         return button
     }()
+    
+    lazy var myImage: UIImageView = {
+        let image = UIImageView(frame: CGRect(x: 8, y: 120, width: 100, height: 200))
+        image.backgroundColor = UIColor.blue
+        return image
+    }()
 
+    lazy var myImage1: UIImageView = {
+        let image = UIImageView(frame: CGRect(x: 150, y: 120, width: 100, height: 200))
+        image.backgroundColor = UIColor.blue
+        return image
+    }()
+    
+    lazy var myImage2: UIImageView = {
+        let image = UIImageView(frame: CGRect(x: 285, y: 120, width: 100, height: 200))
+        image.backgroundColor = UIColor.blue
+        return image
+    }()
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         addSubview(backView)
@@ -52,33 +74,42 @@ class FeedCell: UITableViewCell {
         backView.addSubview(typeLabel)
         backView.addSubview(sizeLabel)
         backView.addSubview(getButton)
-        
+        backView.addSubview(myImage)
+        backView.addSubview(myImage1)
+        backView.addSubview(myImage2)
     }
 }
 
 class InfoModel {
+    
     var appImage: UIImage?
+    
     var appName: String?
+    
     var typeLabel: String?
+    
     var sizeLabel: String?
     
     init(appIamge: UIImage, appName: String, typeLabel: String, sizeLabel: String) {
+        
         self.appImage = appIamge
+        
         self.appName = appName
+        
         self.typeLabel = typeLabel
+        
         self.sizeLabel = sizeLabel
     }
 }
 
 class ViewController: UIViewController {
 
-//    var info = ["APP NAME","Photos & Video","9.26M"]
-
     var infoArray = [InfoModel]()
     
     var tableView = UITableView()
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
         navigationItem.title = "Search"
@@ -86,14 +117,15 @@ class ViewController: UIViewController {
         setTableView()
 
         infoArray.append(InfoModel(appIamge: UIImage(named: "drop")!, appName: "App Name", typeLabel: "Photos & Video", sizeLabel: "9.26M"))
+       
+        infoArray.append(InfoModel(appIamge: UIImage(named: "drop")!, appName: "App Name", typeLabel: "Photos & Video", sizeLabel: "9.26M"))
         
-        print(infoArray)
+        infoArray.append(InfoModel(appIamge: UIImage(named: "drop")!, appName: "App Name", typeLabel: "Photos & Video", sizeLabel: "9.26M"))
 
     }
     
     func setTableView() {
         tableView.frame = self.view.frame
-        tableView.backgroundColor = UIColor.clear
         tableView.delegate = self
         tableView.dataSource = self
         //tableView.separatorColor = UIColor.clear
@@ -125,7 +157,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 120
+        return 350
     }
     
 }
